4cfa80a1d3dee7e0c2c5fb63d38dda66be8af7a6
Anton Shvets <shvets@corp.satel.org>
2020-10-21 14:35:04 +0200

    ngx-cva first example

--- projects/ngx-cva-demo/src/app/app.module.ts
+++ projects/ngx-cva-demo/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputSampleComponent } from './input-sample/input-sample.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputSampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/ngx-cva-demo/src/app/input-sample/input-sample.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ngxCVA, NgxCVA } from 'ngx-cva';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-sample',
+  templateUrl: './input-sample.component.html',
+  styleUrls: ['./input-sample.component.css'],
+  providers: [
+    ngxCVA()
+  ]
+})
+export class InputSampleComponent implements OnInit, NgxCVA {
+  formControl = new FormControl();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
